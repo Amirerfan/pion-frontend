9285f79e8efd2eb8ef9b98f858229257a13104bb
Amirerfan <amirerfan.siamaki@gmail.com>
2023-06-15 18:08:58 +0200

    Connect Wallet Logic

--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -24,13 +24,74 @@ const Navbar = () => {
             Balance: <strong className="ml-2 mr-1">2310.013</strong>
             <strong className="text-xyz-75">PION</strong>
           </button>
-          <ConnectButton />
-          {/*<ConnectButton className="btn btn--small btn--dark-primary">*/}
-          {/*  0x5a03…c7ef*/}
-          {/*</ConnectButton>*/}
+          <ConnectWalletButton />
         </div>
       </div>
     </FadeIn>
+  );
+};
+
+const ConnectWalletButton = () => {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        mounted,
+      }) => {
+        const ready = mounted;
+        const connected = ready && account && chain;
+
+        return (
+          <div
+            {...(!ready && {
+              'aria-hidden': true,
+              style: {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <button
+                    onClick={openConnectModal}
+                    className="btn btn--small btn--dark-primary"
+                  >
+                    Connect Wallet
+                  </button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <button
+                    onClick={openChainModal}
+                    className="btn btn--small btn--secondary"
+                  >
+                    Wrong network
+                  </button>
+                );
+              }
+
+              return (
+                <button
+                  onClick={openAccountModal}
+                  className="btn btn--small btn--dark-primary"
+                >
+                  {account.displayName}
+                </button>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
   );
 };
 
